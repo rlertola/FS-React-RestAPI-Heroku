1028fd0eb2cfcff2de880b2375247a53bb7ef22d
Ryan Lertola <rlertola@gmail.com>
2019-06-12 21:26:42 -0500

    heroku

--- app.js
+++ app.js
@@ -47,7 +47,15 @@ db.once('open', () => {
 
 app.use('/api', routes);
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Friendly greeting for the root route.
 app.get('/', (req, res) => {
@@ -77,9 +85,9 @@ app.use((req, res) => {
 // Set port.
 app.set('port', process.env.PORT || 5000);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
 // Start listening on port.
 const server = app.listen(app.get('port'), () => {
